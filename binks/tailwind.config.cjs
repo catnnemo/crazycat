72ee8dbdae021d0d87e1d5d01e986404491d26c9
catnnemo <njpianoforte@gmail.com>
2023-03-01 16:08:47 -0500

    added helper file and main style

---
+++ binks/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
+	theme: {
+		fontFamily:{
+			title: ['"Paytone One"','sans-serif'],
+		},
+		extend: {
+		},
+	},
+	plugins: [],
+}
